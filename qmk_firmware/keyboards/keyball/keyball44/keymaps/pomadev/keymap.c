c160eabc55bb6ad07bbd05f7924ad32c8e03c78e
pomadev <y.pomadev@gmail.com>
2024-11-29 22:41:44 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/pomadev/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/pomadev/keymap.c
@@ -55,15 +55,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
-
-    #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
-    if (get_highest_layer(remove_auto_mouse_layer(state, true)) == 3) {
-      state = remove_auto_mouse_layer(state, false);
+    if (get_highest_layer(state) == 3) {
+      auto_mouse_layer_off();
       keyball_set_scroll_mode(true);
     }
-    #endif
-
     return state;
 }
 
